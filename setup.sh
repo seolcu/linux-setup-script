9d538cf901381e038568435de919c8e17901ac69
seolcu <seolcu0112@proton.me>
2023-07-14 03:26:55 +0900

    vscode install fixed

--- setup.sh
+++ setup.sh
@@ -9,12 +9,17 @@ sudo apt full-upgrade -y
 
 
 # install essential packages
-sudo apt install -y git gcc g++ curl htop neofetch python3-nautilus
+sudo apt install -y git gcc g++ curl wget gpg htop neofetch python3-nautilus
 
 
 # install vscode
-curl -o vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo apt install ./vscode.deb
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 
 # debloat
