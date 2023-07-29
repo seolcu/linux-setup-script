288eedd00a88f352894f58b4015bfd14891d320a
seolcu <seolcu0112@proton.me>
2023-07-30 00:01:43 +0900

    register accept key -> no select

--- package_list.py
+++ package_list.py
@@ -35,6 +35,8 @@ class package_list:
             map(lambda a_package: a_package.name, self.raw_package_list),
             multi_select=True,
             show_multi_select_hint=True,
+            multi_select_select_on_accept=False,
+            multi_select_empty_ok=True,
         ).show()
 
     def __init__(self, raw_package_list: list[package]):
