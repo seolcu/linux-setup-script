6a01cabe79f557afd0b4bebb33187d94af700306
seolcu <seolcu0112@proton.me>
2023-12-23 15:40:55 +0900

    hotfix: setup first

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import helpers
 import setup
 
 if __name__ == "__main__":
+    setup.setup()
     helpers.display_title("Welcome to the package manager")
     helpers.display_title("Setting up the environment")
-    setup.setup()
     helpers.display_question("What distro are you using?")
     distro = helpers.select_one_string("arch", "debian")
     if distro == "arch":
