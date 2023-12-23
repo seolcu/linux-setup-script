12bd7a25bb9dbbe7884bb4244f01709146125b3e
seolcu <seolcu0112@proton.me>
2023-12-23 15:50:58 +0900

    hotfix: fixed type bug

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     helpers.display_title("Welcome to the package manager")
     helpers.display_title("Setting up the environment")
     helpers.display_question("What distro are you using?")
-    distro = helpers.select_one_string("arch", "debian")
+    distro = helpers.select_one_string(["arch", "debian"])
     if distro == "arch":
         import arch
 
