3566513c4ab7c1205b1739ec59c5995d79db9ccb
seolcu <seolcu0112@proton.me>
2024-06-09 03:48:00 +0900

    added fastfetch config

--- common.sh
+++ common.sh
@@ -14,7 +14,7 @@ read answer
 
 if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-    echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+    echo -e '\n. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
     echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
     source ~/.bashrc
     asdf plugin add protonge
@@ -36,10 +36,38 @@ if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
     git config --global push.autoSetupRemote true
 fi
 
+echo -n "Add nord colorscheme to Konsole? [y/N]: "
+
+read answer
+
+if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
+    mkdir -p ~/.local/share/konsole
+    curl -o ~/.local/share/konsole/Nord.colorscheme https://raw.githubusercontent.com/nordtheme/konsole/develop/src/nord.colorscheme
+fi
+
+echo -n "Load fastfetch configuration? [y/N]: "
+
+read answer
+
+if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
+    mkdir -p ~/.config/fastfetch
+    curl -o ~/.config/fastfetch/config.jsonc https://raw.githubusercontent.com/fastfetch-cli/fastfetch/dev/presets/examples/10.jsonc
+fi
+
+echo -n "Add simplified fastfetch to .bashrc? [y/N]: "
+
+read answer
+
+if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
+    mkdir -p ~/.config/fastfetch/presets
+    curl -o ~/.config/fastfetch/presets/9.jsonc https://raw.githubusercontent.com/fastfetch-cli/fastfetch/dev/presets/examples/9.jsonc
+    echo -e "\nfastfetch -c ~/.config/fastfetch/presets/9.jsonc" >> ~/.bashrc
+fi
+
 echo -n "Add starship init to .bashrc? [y/N]: "
 
 read answer
 
 if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]; then
-    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+    echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
 fi
